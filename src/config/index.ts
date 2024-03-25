87478eac25e3e097f5aebb8d21b47b542b9ee67f
Gevorg Abgaryan <gevorg.gak@gmail.com>
2024-03-25 10:40:39 +0400

    README and tech fixes

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,12 +6,12 @@ const config = {
   JWTSecret: process.env.JWT_SECRET || '',
   JWTExpireIn: process.env.JWT_EXPIRE_IN,
   mongoDB: {
-    url: process.env.MONGO_DB_URL,
-    dbName: process.env.MONGO_DB_NAME,
+    url: process.env.MONGO_DB_URL || 'mongodb://localhost:27019',
+    dbName: process.env.MONGO_DB_NAME || 'CHAT',
   },
   defaultRoomId: '',
   wsPort: process.env.WS_PORT ? parseInt(process.env.WS_PORT, 10) : 1990,
-  redisUrl: `redis://localhost:${process.env.REDIS_PORT ?? 6379}`
+  redisUrl: process.env.REDIS_URL || 'redis://localhost:8389'
 };
 
 export default config;
